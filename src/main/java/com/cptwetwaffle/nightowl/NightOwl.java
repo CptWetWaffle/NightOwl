6412876994aea188b73b7bec1d40b82d231a1ef7
John Sousa <douglas.sousa@gmail.com>
2017-09-17 01:58:25 +0100

    Mod info
    Got versions working

---
+++ src/main/java/com/cptwetwaffle/nightowl/NightOwl.java
@@ -0,0 +1,27 @@
+package com.cptwetwaffle.nightowl;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "nightowl", name = "Night Owl", version = "1.12.1-1.0")
+public class NightOwl {
+    @Mod.Instance
+    public static NightOwl instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
